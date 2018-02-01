047bcc3dd26e18d555b39f3b5d8fd2b8cbe6f3fb
Sideway <dev.sideway@gmail.com>
2018-02-01 13:55:52 +0100

    Added basic tokenization (only missing byte arrays)

--- include/asm/lexer.hpp
+++ include/asm/lexer.hpp
@@ -9,10 +9,21 @@
 namespace Asm {
     class Lexer {
         public:
+            Lexer();
+            Lexer(std::string file);
 
+            void tokenizeFile(std::string path);
+            void tokenizeString(std::string data);
+            
+            void appendToken(Token t);
+            void appendToken(std::string tokstr); // Determines the token type and adds it to the tokenlist
+
+            Token getNext(); // Returns next token from stream or TokenType::Eof if we reached the end
+            void rewind(); // Rewind the token pointer to the start of the stream
         private:
             std::vector<std::string> lines; // Used for error messages
             std::vector<Token> tokens;
+            std::vector<Token>::iterator it;
 
     };
 }
--- src/asm/lexer.cpp
+++ src/asm/lexer.cpp
@@ -1,1 +1,138 @@
+#include "asm/lexer.hpp"
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <cctype>
+#include <iostream>
+
+static char getEscape(char code)
+{
+    switch(code){
+        case 'n':
+            return '\n';
+            break;
+        case 't':
+            return '\t';
+            break;
+        case 'r':
+            return '\r';
+            break;
+        default:
+            return code;
+    }
+}
+
+namespace Asm {
+    Lexer::Lexer()
+    {
+        it = tokens.begin();
+    }
+
+    Lexer::Lexer(std::string path)
+    {
+        tokenizeFile(path);
+    }
+
+    void Lexer::appendToken(std::string data)
+    {
+        // For debug we add everything as a symbol
+        std::cout << "Token : " << data << std::endl;
+        tokens.push_back(Token(TokenType::Symbol,data));
+    }
+
+    void Lexer::appendToken(Token t)
+    {
+        tokens.push_back(t);
+    }
+
+    void Lexer::tokenizeFile(std::string path)
+    {
+        std::fstream fd;
+        std::stringstream s;
+
+        fd.open(path, std::fstream::in);
+
+        if(!fd.is_open()){
+            throw std::runtime_error("Could not open input file");
+        }
+
+        s << fd.rdbuf();
+        fd.close();
+
+        tokenizeString(s.str());
+    }
+
+    void Lexer::tokenizeString(std::string data)
+    {
+        std::string current_token;
+        
+        // State -1 : Not in quoted string
+        // State 0 : In quoted string
+        // State 1 : Escape sequence
+        int state = -1;
+        int row = 1;
+        int col = 0;
+        char current_char;
+        
+        for(std::string::iterator it = data.begin(); it != data.end(); ++it){
+            current_char = *it;
+
+            if(state >= 0){
+                switch(state){
+                    case 0:
+                        if(current_char == '"'){
+                            state = -1;
+                            appendToken(current_token);
+                            current_token.clear();
+                        }else if(current_char == '\\'){
+                            state = 1;
+                        }else{
+                            current_token += current_char;
+                        }
+                        break;
+                    case 1:
+                        current_token += getEscape(current_char);
+                        state = 0;
+                    default:
+                        break;
+                        // Do nothing, maybe exception ?
+                }
+            }else{
+            
+                if(isspace(current_char)){
+                    if(current_token.length() > 0){
+                        appendToken(current_token);
+                        current_token.clear();
+                    }
+                }else{
+                    switch(current_char){
+                        case ';': // Comment
+                            while(*it != '\n' && it != data.end()) // Skips comments
+                                ++it;
+                            
+                            row++;
+                            col = 0;
+                            break;
+                        case '\n':
+                            col = 0;
+                            row++;
+                            break;
+                        case '"':
+                            state = 0;
+                            break;
+                        default:
+                            current_token += current_char;
+                            break;
+                    }
+                }
+            }
+       }
+
+        if(current_token.length() > 0)
+            appendToken(current_token); 
+    }
+}
