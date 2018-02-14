56f256c7c8e7d42077a21c33eb7da3fcbee79f24
Sideway <dev.sideway@gmail.com>
2018-02-14 16:28:24 +0100

    Added builder in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 #include <string>
 
 #include "asm/lexer.hpp"
+#include "asm/token.hpp"
+#include "codegen/builder.hpp"
 
 int main(int argc, char **argv)
 {
     try{
         Asm::Lexer l;
         l.tokenizeFile("./samples/hello_world.min");
+        Codegen::Builder builder(l);
+        builder.compile("a.mx");
     }catch(std::exception &e){
         std::cerr << "Err : " << e.what() << std::endl;
     }
