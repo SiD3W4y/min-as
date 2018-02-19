4b615ef88ff0b6a2f13ef49ff316e77e27714c38
Sideway <dev.sideway@gmail.com>
2018-02-19 23:04:54 +0100

    Added ops header

---
+++ include/codegen/ops.hpp
@@ -0,0 +1,10 @@
+#ifndef OPS_HPP
+#define OPS_HPP
+
+#include <unordered_map>
+#include <string>
+
+extern std::unordered_map<std::string, int> opcodes;
+extern std::unordered_map<std::string, int> registers;
+
+#endif
