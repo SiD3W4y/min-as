511e682669872ed92ee8858483e00a4dec3fda9d
Sideway <dev.sideway@gmail.com>
2018-02-20 19:28:08 +0100

    Added info to opcode map (OpInfo struct)

--- include/codegen/ops.hpp
+++ include/codegen/ops.hpp
@@ -4,7 +4,14 @@
 #include <unordered_map>
 #include <string>
 
-extern std::unordered_map<std::string, int> opcodes;
-extern std::unordered_map<std::string, int> registers;
+namespace Codegen {
 
+    struct OpInfo {
+        int opcode;
+        int arg_count;
+    };
+
+    extern std::unordered_map<std::string, OpInfo> opcodes;
+    extern std::unordered_map<std::string, int> registers;
+}
 #endif
--- src/codegen/ops.cpp
+++ src/codegen/ops.cpp
@@ -5,31 +5,31 @@
 
 namespace Codegen { 
     // TODO : Add call and ret to min-vm
-    std::unordered_map<std::string, int> opcodes = {
-        {"add", 0x00},
-        {"sub", 0x01},
-        {"mul", 0x02},
-        {"mov", 0x03},
-        {"ldr", 0x04},
-        {"ldrb", 0x05},
-        {"str", 0x06},
-        {"strb", 0x07},
-        {"push", 0x08},
-        {"pop", 0x09},
-        {"cmp", 0x0a},
-        {"jmp", 0x0b},
-        {"jne", 0x0c},
-        {"je", 0x0d},
-        {"jle", 0x0e},
-        {"jbe", 0x0f},
-        {"sys", 0x10},
-        {"xor", 0x11},
-        {"and", 0x12},
-        {"or", 0x13},
-        {"shr", 0x14},
-        {"shl", 0x15},
-        {"call", 0x16},
-        {"ret", 0x17}
+    std::unordered_map<std::string, OpInfo> opcodes = {
+        {"add", {0x00, 2}},
+        {"sub", {0x01, 2}},
+        {"mul", {0x02, 2}},
+        {"mov", {0x03, 2}},
+        {"ldr", {0x04, 2}},
+        {"ldrb", {0x05, 2}},
+        {"str", {0x06, 2}},
+        {"strb", {0x07, 2}},
+        {"push", {0x08, 1}},
+        {"pop", {0x09, 1}},
+        {"cmp", {0x0a, 2}},
+        {"jmp", {0x0b, 1}},
+        {"jne", {0x0c, 1}},
+        {"je", {0x0d, 1}},
+        {"jle", {0x0e, 1}},
+        {"jbe", {0x0f, 1}},
+        {"sys", {0x10, 0}},
+        {"xor", {0x11, 2}},
+        {"and", {0x12, 2}},
+        {"or", {0x13, 2}},
+        {"shr", {0x14, 2}},
+        {"shl", {0x15, 2}},
+        {"call", {0x16, 1}},
+        {"ret", {0x17, 0}}
     };
 
     std::unordered_map<std::string, int> registers = {
